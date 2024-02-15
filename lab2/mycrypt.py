292f4c34e9d5592c87274e1ef7d009f7217a3330
Oliver <oliver.saari@student.oulu.fi>
2024-02-15 22:36:09 +0200

    optimizing

--- lab2/mycrypt.py
+++ lab2/mycrypt.py
@@ -5,12 +5,13 @@ def encode(s):
         raise TypeError
     origlen = len(s)
     #Pad the string to max length
-    s += "x" * (1000 - len(s))
     crypted = ""
     digitmapping = dict(zip('1234567890!"#€%&/()=','!"#€%&/()=1234567890'))
     invalid_char = {'\xe5', '\xe4', '\xf6'}
     if len(s) > 1000:
         raise ValueError
+    else:
+        s += "x" * (1000 - len(s))
     for c in s:
         if c in invalid_char:
             #Character is invalid
@@ -19,9 +20,9 @@ def encode(s):
             if c.islower():
                 c=c.upper()
             # Rot13 the character for maximum security
-            crypted+=codecs.encode(c,'rot13')
+            crypted += codecs.encode(c,'rot13')
         elif c in digitmapping:
-          crypted+=digitmapping[c]
+          crypted += digitmapping[c]
         #else, the character is invalid
         else:
             raise ValueError
